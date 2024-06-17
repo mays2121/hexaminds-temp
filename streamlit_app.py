c7a0ca5755a81c24f9aa040956b4b97c73f22083
Ratri Mayangsari <18159668+mays2121@users.noreply.github.com>
2024-06-17 19:20:52 +0200

    Update model gpt

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ def main():
     if csv_file is not None:
 
         agent = create_csv_agent(
-            OpenAI(temperature=0), csv_file, verbose=True, model="gpt-4o")
+            OpenAI(temperature=0), csv_file, verbose=True)
 
         user_question = st.text_input("Ask a question about your file: ")
 
